b261be957248ea571351cd9863c808c431334879
Chris Kwok <chris.kwok@climate.com>
2015-12-29 13:36:12 +0800

    add action to cell on selected

--- ParkingMeow/ViewControllers/ParkingDetailsTableViewController.swift
+++ ParkingMeow/ViewControllers/ParkingDetailsTableViewController.swift
@@ -20,7 +20,6 @@ class ParkingDetailsTableViewController: UITableViewController {
         return f
     }()
 
-
     let rateDisplayNames = [
         ParkingRate.RateType.OneHour : "1 HR",
         ParkingRate.RateType.TwoHour : "2 HR",
@@ -34,66 +33,15 @@ class ParkingDetailsTableViewController: UITableViewController {
         ParkingBusinessHour.HourType.Sun : "Sun",
     ]
 
-    var rateCellContentTuples : [(String , String?)]?
-    var businessHourCellContentTuples : [(String , String?)]?
-    var detailContentTuples : [(String, String?)]?
-
+    var rateCellContentTuples: [(name: String , val: String?, action: (Void -> Void)?)]?
+    var businessHourCellContentTuples: [(name: String , val: String?, action: (Void -> Void)?)]?
+    var detailCellContentTuples: [(name: String , val: String?, action: (Void -> Void)?)]?
 
     var parkingLot : ParkingLot? {
         didSet {
-            rateCellContentTuples = {
-                var tuples = [(String , String?)]()
-                if let rates = parkingLot?.parkingRates {
-                    for rate in rates {
-                        var priceStr = notAvailableStr
-                        if let rateType = rate.rateType, displayName = rateDisplayNames[rateType] {
-                            if let price = rate.price where Double(price) > 0 {
-                                priceStr = numberFormatter.stringFromNumber(price)!
-                            }
-                            tuples.append((displayName, priceStr))
-                        }
-                    }
-                }
-                return tuples
-            }()
-
-            businessHourCellContentTuples =  {
-                var tuples = [(String , String?)]()
-                if let hours = parkingLot?.parkingBusinessHours {
-                    for hour in hours {
-                        if let fromTo = hour.fromTo, hourType = hour.hourType, displayName = businessHourDisplayNames[hourType] {
-                            tuples.append((displayName, fromTo))
-                        }
-                    }
-                }
-                return tuples
-            }()
-
-            detailContentTuples = {
-                // name
-                let nameVal = (parkingLot?.webname != nil) ? parkingLot?.webname : (parkingLot?.facName != nil) ? parkingLot?.facName : parkingLot?.opName
-                let name : (String, String?) = ("name", nameVal)
-                //address
-                let address : (String, String?) = ("address", parkingLot?.deaFacilityAddress)
-                // web
-                let web : (String, String?) = ("web", parkingLot?.opWeb)
-                // phone
-                let phoneVal = (parkingLot?.opPhone != nil) ? parkingLot?.opPhone : parkingLot?.opPhone2
-                let phone : (String, String?) = ("phone", phoneVal)
-                // stall
-                let stallVal : String? = (parkingLot?.deaStalls != nil) ? String((parkingLot?.deaStalls)!) : nil
-                let stall : (String, String?) = ("total stalls", stallVal)
-                // disabled
-                let disabledVal : String? = (parkingLot?.disabled != nil) ? String((parkingLot?.disabled)!) : nil
-                let disabled : (String, String?) = ("total disabled", disabledVal)
-                // vacant
-                let vacantVal : String? = (parkingLot?.vacant != nil) ? String((parkingLot?.vacant)!) : nil
-                let vacant : (String, String?) = ("total vacant", vacantVal)
-                // all
-                let tuples : [(String, String?)]? = [name, address, web, phone, stall, disabled, vacant]
-                return tuples
-            }()
-
+            rateCellContentTuples = getRateCellContentTuples()
+            businessHourCellContentTuples = getBusinessHourCellContentTuples()
+            detailCellContentTuples = getDetailCellContentTuples()
             tableView.reloadData()
         }
     }
@@ -113,17 +61,108 @@ class ParkingDetailsTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    private func getContentTuples(section : Int) -> [(String, String?)]? {
+    private func getContentTuples(section : Int) -> [(name: String, val: String?, action: (Void -> Void)?)]? {
         switch section {
         case 0:
             return rateCellContentTuples
         case 1:
             return businessHourCellContentTuples
         case 2:
-            return detailContentTuples
+            return detailCellContentTuples
         default:
             return nil
         }
+    }
+
+    private func getRateCellContentTuples() -> [(name: String , val: String?, action: (Void -> Void)?)] {
+        var tuples: [(name: String , val: String?, action: (Void -> Void)?)] = []
+        if let rates = parkingLot?.parkingRates {
+            for rate in rates {
+                var priceStr = notAvailableStr
+                if let rateType = rate.rateType, displayName = rateDisplayNames[rateType] {
+                    if let price = rate.price where Double(price) > 0 {
+                        priceStr = numberFormatter.stringFromNumber(price)!
+                    }
+                    tuples.append((name: displayName, val: priceStr, action: nil))
+                }
+            }
+        }
+        return tuples
+    }
+
+    private func getBusinessHourCellContentTuples() -> [(name: String , val: String?, action: (Void -> Void)?)] {
+        var tuples: [(name: String , val: String?, action: (Void -> Void)?)] = []
+        if let hours = parkingLot?.parkingBusinessHours {
+            for hour in hours {
+                if let fromTo = hour.fromTo, hourType = hour.hourType, displayName = businessHourDisplayNames[hourType] {
+                    tuples.append((displayName, fromTo, nil))
+                }
+            }
+        }
+        return tuples
+    }
+
+    private func getDetailCellContentTuples() -> [(name: String , val: String?, action: (Void -> Void)?)] {
+        // name
+        let nameVal = (parkingLot?.webname != nil) ? parkingLot?.webname : (parkingLot?.facName != nil) ? parkingLot?.facName : parkingLot?.opName
+        let name: (String, String?, (Void -> Void)?) = ("name", nameVal, nil)
+        //address
+        let address: (String, String?, (Void -> Void)?) = ("address", parkingLot?.deaFacilityAddress, nil)
+        // web
+        var webAction: (Void -> Void)? = nil
+        if let urlString = self.parkingLot?.opWeb, url = NSURL(string: urlString) {
+            webAction = {
+                let alertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                    UIApplication.sharedApplication().openURL(url)
+                })
+                let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+                let vc: UIAlertController = UIAlertController(title: "Open with web browser?", message: "", preferredStyle: .Alert)
+                vc.addAction(alertAction)
+                vc.addAction(cancelAction)
+                self.presentViewController(vc, animated: true, completion: nil)
+            }
+        }
+        let web: (String, String?, (Void -> Void)?) = ("web", parkingLot?.opWeb, webAction)
+        // phone
+        var phoneAction: (Void -> Void)? = nil
+        let phoneVal = (parkingLot?.opPhone != nil) ? parkingLot?.opPhone : parkingLot?.opPhone2
+        if let urlString = phoneVal {
+            let replaced = String(urlString.characters.map {
+                if $0 == "(" || $0 == ")" || $0 == "-" {
+                    return " "
+                } else {
+                    return $0
+                }
+            })
+            let noWhiteSpace = replaced.stringByReplacingOccurrencesOfString(" ", withString: "")
+            let url = NSURL(string: "tel://" + noWhiteSpace)
+            phoneAction = {
+                let alertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                    if UIApplication.sharedApplication().canOpenURL(url!) {
+                        UIApplication.sharedApplication().openURL(url!)
+                    }
+                })
+                let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+                let vc: UIAlertController = UIAlertController(title: "Open with phone app?", message: "", preferredStyle: .Alert)
+                vc.addAction(alertAction)
+                vc.addAction(cancelAction)
+                self.presentViewController(vc, animated: true, completion: nil)
+            }
+        }
+
+        let phone: (String, String?, (Void -> Void)?) = ("phone", phoneVal, phoneAction)
+        // stall
+        let stallVal : String? = (parkingLot?.deaStalls != nil) ? String((parkingLot?.deaStalls)!) : nil
+        let stall: (String, String?, (Void -> Void)?) = ("total stalls", stallVal, nil)
+        // disabled
+        let disabledVal: String? = (parkingLot?.disabled != nil) ? String((parkingLot?.disabled)!) : nil
+        let disabled: (String, String?, (Void -> Void)?) = ("total disabled", disabledVal, nil)
+        // vacant
+        let vacantVal: String? = (parkingLot?.vacant != nil) ? String((parkingLot?.vacant)!) : nil
+        let vacant: (String, String?, (Void -> Void)?) = ("total vacant", vacantVal, nil)
+        // all
+        let tuples: [(name: String, val: String?, action: (Void -> Void)?)] = [name, address, web, phone, stall, disabled, vacant]
+        return tuples
     }
 
 
@@ -158,6 +197,17 @@ class ParkingDetailsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sectionNames[section]
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let section = indexPath.section
+        let row = indexPath.item
+        guard let tuples = getContentTuples(section) else {
+            return
+        }
+        if let action = tuples[row].action {
+            action()
+        }
     }
 
     /*
